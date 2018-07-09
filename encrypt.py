56ce9a2fa4aeb52091528a962abc7157f15e4443
yukunxie <jinyun2007@126.com>
2018-07-09 14:21:27 +0800

    add command line

--- encrypt.py
+++ encrypt.py
@@ -15,20 +15,29 @@ import os
 import time
 import types
 import random
+import argparse
 
+random.seed(int(time.time()))
 
 def gen_random_symbol(symbol_dict, key):
     if key.startswith("@@"):
         return key
-    if key in symbol_dict:
-        return symbol_dict[key]
     seed = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+=-"
-    while 1:
-        value = "@@" + "".join(random.sample(seed, random.randint(10,15)))
+
+    # gen short random name
+    for i in xrange(100):
+        value = "@" + "".join(random.sample(seed, random.randint(10,15))) + "@"
         if value not in symbol_dict:
-            symbol_dict[key] = value
             symbol_dict[value] = key
             return value
+
+    # gen a long random name
+    while 1:
+        value = "@" + "".join(random.sample(seed, random.randint(15,20))) + "@"
+        if value not in symbol_dict:
+            symbol_dict[value] = key
+            return value
+
 
 def load_symbol_file(filename):
     symbol_dict = {}
@@ -42,33 +51,15 @@ def load_symbol_file(filename):
                 continue
             (key, value) = text.split(" ")
             symbol_dict[key] = value
-            symbol_dict[value] = key
     return symbol_dict
 
 def save_symbol_file(filename, symbol_dict):
     with open(filename, "w") as fd:
         fd.truncate(0)
         for k, v in symbol_dict.iteritems():
-            if k.startswith("@@"):
-                continue
-            fd.write("%s %s\n" % (v, k))
+            fd.write("%s %s\n" % (k, v))
         fd.close()
 
-
-
-def process_file(fname, symbol_dict):
-    with open(fname, "rb") as f:
-        magic = f.read(4)
-        moddate = f.read(4)
-        code = marshal.load(f)
-
-    new_code = gen_new_code(code, symbol_dict)
-
-    with open(fname, "wb") as fd:
-        fd.truncate(0)
-        data = magic + moddate + marshal.dumps(new_code)
-        fd.write(data)
-     
 def gen_new_code(code, symbol_dict):
     varnames = []
     for i, varname in enumerate(code.co_varnames):
@@ -96,14 +87,60 @@ def gen_new_code(code, symbol_dict):
                              consts,             #code.co_consts,
                              code.co_names,
                              varnames,            #code.co_varnames,
-                             code.co_filename,
-                             code.co_name,
+                             gen_random_symbol(symbol_dict, code.co_filename), #code.co_filename,
+                             gen_random_symbol(symbol_dict, code.co_name),#code.co_name,
                              code.co_firstlineno,
                              code.co_lnotab,       # In general, You should adjust this
                              code.co_freevars,
                              code.co_cellvars)
     return opt_code
 
-symbol_dict = load_symbol_file("symbols.txt")
-process_file(sys.argv[1], symbol_dict) 
-save_symbol_file("symbols.txt", symbol_dict)
+'''
+*** get all files with special suffix in root_dir
+'''
+def get_all_files(root_dir, suffix = []):
+    ret = []
+    for root, dirs, files in os.walk(root_dir):
+        if root.find(".svn") >= 0:
+            continue
+        for file in files:
+            if not suffix:
+                ret.append(os.path.join(root,file))
+            else:
+                base, suf = os.path.splitext(file)
+                if suf in suffix:
+                    ret.append(os.path.join(root,file))
+    return ret
+
+
+def process_file(fname, symbol_dict):
+    print ">>> process file:", fname
+    with open(fname, "rb") as f:
+        magic = f.read(4)
+        moddate = f.read(4)
+        code = marshal.load(f)
+    new_code = gen_new_code(code, symbol_dict)
+    with open(fname, "wb") as fd:
+        fd.truncate(0)
+        data = magic + moddate + marshal.dumps(new_code)
+        fd.write(data)
+
+def process_dir(fdir, symbol_dict):
+    for file in get_all_files(fdir, [".pyc"]):
+        process_file(file, symbol_dict)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Process some inputs.')
+    parser.add_argument('--input', help='the target pyc or direcotry')
+    parser.add_argument('--symbol', help='the symbols file path')
+    args = parser.parse_args()
+
+    symbol_dict = load_symbol_file(args.symbol)
+    if os.path.isdir(args.input):
+        process_dir(args.input, symbol_dict)
+    else:
+        process_file(args.input, symbol_dict)
+    save_symbol_file( args.symbol, symbol_dict)
+
+    print "done."
